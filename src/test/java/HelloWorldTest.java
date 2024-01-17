26288917fd4493444b2e45f2a27879654c2c6d7b
Dimateg <dimateg@gmail.com>
2024-01-17 21:43:41 +0300

    EX5

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,6 +1,11 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.http.Headers;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloWorldTest {
 
@@ -11,7 +16,7 @@ public class HelloWorldTest {
     }
 
     @Test
-    public void TestHello(){
+    public void TestHello() {
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/hello")
                 .andReturn();
@@ -24,5 +29,67 @@ public class HelloWorldTest {
                 .get("https://playground.learnqa.ru/api/get_text")
                 .andReturn();
         response.prettyPrint();
+    }
+
+    @Test
+    public void testRestAssured() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "John");
+
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+
+        String name = response.get("answer2");
+        if (name == null) {
+            System.out.println("The key 'answer2' is absent");
+        } else {
+            System.out.println(name);
+        }
+    }
+
+    @Test
+    public void testRestAssured2() {
+        Map<String, String> data = new HashMap<>();
+        data.put("login", "secret_login2");
+        data.put("password", "secret_pass2");
+
+        Response responseForGet = RestAssured
+                .given()
+                .body(data)
+                .when()
+                .post("https://playground.learnqa.ru/api/get_auth_cookie")
+                .andReturn();
+
+        String responseCookie = responseForGet.getCookie("auth_cookie");
+
+        Map<String, String> cookies = new HashMap<>();
+        if (responseCookie != null){
+            cookies.put("auth_cookie", responseCookie);
+        }
+
+        Response responseForCheck = RestAssured
+                .given()
+                .body(data)
+                .cookies(cookies)
+                .when()
+                .post("https://playground.learnqa.ru/api/check_auth_cookie")
+                .andReturn();
+
+        responseForCheck.print();
+    }
+
+    @Test
+    public void testEx5() {
+        JsonPath responseJSONHomework = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        String answer = responseJSONHomework.getString("messages[1].message");
+        System.out.println(answer);
+
     }
 }
